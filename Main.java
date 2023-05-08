e38e8ad4d42cacc81ddb0aade11bd7fc7b2ec181
Maciek <goralczykmaciek@gmail.com>
2023-05-08 15:55:05 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+package World_sim;
+public class Main {
+    public static void main(String[] args)
+    {
+        Simulator simulator = new Simulator();
+
+    }
+}
---
+++ Simulator.java
@@ -0,0 +1,13 @@
+package World_sim;
+
+import javax.swing.*;
+
+public class Simulator {
+    public void Simulator() {
+        JFrame window = new JFrame("193302 Maciej Góralczyk");
+        window.setVisible(true);
+        window.setSize(900, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
